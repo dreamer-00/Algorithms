5994617ddbff55038d745664e3d9047e60bac15d
Vatsal Pathak <pathakvatsal86@gmail.com>
2026-04-14 21:51:35 +0530

    2220A Solved

--- CodeForces/2220A.cpp
+++ CodeForces/2220A.cpp
@@ -1,13 +1,31 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 int main(){
     int t; cin>>t;
     while(t--){
         int n; cin>>n;
         vector<int> a(n);
+        bool possible=true;
         for(int i=0; i<n; i++){
             cin>>a[i];
         }
-        for(int i=0; i<n; i++)
+        sort(a.rbegin(), a.rend());
+        for(int i=0; i<n-1; i++){
+            if(a[i]==a[i+1]){
+                possible=false;
+            }
+        }
+        if(!possible){
+            cout<<-1<<"\n";
+        }
+        else{
+            for(int i=0; i<n; i++){
+                cout<<a[i]<<" ";
+            }
+            cout<<"\n";
+        }
     }
+    return 0;
 }
