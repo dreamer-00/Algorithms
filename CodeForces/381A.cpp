29af0f42d7a44a635b46618c35da810ae87062be
Vatsal Pathak <pathakvatsal86@gmail.com>
2026-04-15 14:14:19 +0530

    381A Solved

---
+++ CodeForces/381A.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main(){
+    int n; cin>>n;
+    vector<int> a(n);
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    int left=0;
+    int right=n-1;
+    int sumA=0;
+    int sumB=0;
+    bool turn =true;
+    while(left<=right){
+        int picked;
+        if(a[left]>a[right]){
+            picked=left;
+            left++;
+        }
+        else{
+            picked=right;
+            right--;
+        }
+        if(turn){
+            sumA+=picked;
+        }
+        else{
+            sumB+=picked;
+        }
+        turn=!turn;
+    }
+    cout<<sumA<<" "<<sumB<<"\n";
+    return 0;
+}
